7e07cb00475873f69cf2a67341804d0eb5db09af
Burak Yunkul <byunkul23@hotmail.com>
2022-11-22 20:42:54 +0300

    Add project files.

---
+++ EfCoreDifferentDatabaseUse/Entities/Album.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace EfCoreDifferentDatabaseUse.Entities
+{
+	[Table("Albums")]
+	public class Album
+	{
+		[Key]
+		public int Id { get; set; }
+
+		[StringLength(100)]
+		public string? Name { get; set; }
+
+		[StringLength(100)]
+		public string? Description { get; set; }
+	}
+}
---
+++ EfCoreDifferentDatabaseUse/Program.cs
@@ -0,0 +1,36 @@
+namespace EfCoreDifferentDatabaseUse
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+
+			// Add services to the container.
+			builder.Services.AddControllersWithViews();
+
+			// TODO : Set database provider
+			//builder.Services.AddDbContext<DatabaseContext>();
+
+
+			var app = builder.Build();
+
+			// Configure the HTTP request pipeline.
+			if (!app.Environment.IsDevelopment())
+			{
+				app.UseExceptionHandler("/Home/Error");
+			}
+			app.UseStaticFiles();
+
+			app.UseRouting();
+
+			app.UseAuthorization();
+
+			app.MapControllerRoute(
+				name: "default",
+				pattern: "{controller=Home}/{action=Index}/{id?}");
+
+			app.Run();
+		}
+	}
+}
